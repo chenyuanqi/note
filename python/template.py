a2405e9973b56a18e5bb26e28db5806aa70a24f3
chenyuanqi <chenyuanqi90s@163.com>
2018-02-23 11:45:00 +0800

    add some directory for next todo.

---
+++ python/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by vikey on ${DATE}
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
