c88dffdf40109c33fd3e838eea4891781031f41c
chenyuanqi <chenyuanqi@outlook.com>
2022-05-09 10:47:10 +0800

    update: add blog form.

--- backend/go/reborn/project/blog/src/main.go
+++ backend/go/reborn/project/blog/src/main.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// 语法错误：non-declaration statement outside function body 函数外无法使用变量赋值语句
+// router := mux.NewRouter()
+// 包级别的变量声明时不能使用 := 语法，修改为带关键词 var 的变量声明即可
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到主页</h1>")
@@ -34,6 +39,29 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "访问文章列表~")
+}
+
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "创建博文表单")
+	// 多行字符串使用 ``
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+	// 获取创建博文的链接，使用命名路由的好处是为 URL 修改提供了灵活性
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
@@ -63,15 +91,18 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-	router := mux.NewRouter()
+	// router := mux.NewRouter()
 	// .StrictSlash(true) 处理 url 尾部 /
 	// router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
+	// 查看博文
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	// 创建博文
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
 	// 自定义 404 页面
@@ -81,10 +112,10 @@ func main() {
 	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
+	// homeURL, _ := router.Get("home").URL()
+	// fmt.Println("homeURL: ", homeURL)
+	// articleURL, _ := router.Get("articles.show").URL("id", "23")
+	// fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":8000", removeTrailingSlash(router))
 }
