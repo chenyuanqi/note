93d3633b9caa43b3042b6e2a0b328f5c8dcdfc30
chenyuanqi <chenyuanqi@outlook.com>
2022-05-10 18:07:28 +0800

    update

--- backend/go/reborn/project/blog/routes/web.go
+++ backend/go/reborn/project/blog/routes/web.go
@@ -7,14 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Router 路由对象
-var Router *mux.Router
-
-// Initialize 初始化路由
-func Initialize() {
-	Router = mux.NewRouter()
-}
-
 // RegisterWebRoutes 注册网页相关路由
 func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面
