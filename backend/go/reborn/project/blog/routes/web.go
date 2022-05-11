9260c9aeaa663a8d6f7de2e2d659442695fb4613
chenyuanqi <chenyuanqi@outlook.com>
2022-05-11 17:26:02 +0800

    update: 美化文章列表页

--- backend/go/reborn/project/blog/routes/web.go
+++ backend/go/reborn/project/blog/routes/web.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"blog/app/http/controllers"
-	"blog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -30,6 +29,10 @@ func RegisterWebRoutes(r *mux.Router) {
 	// 删除博文
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
+	// 静态资源
+	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
+
 	// 中间件：强制内容类型为 HTML
-	r.Use(middlewares.ForceHTML)
+	// r.Use(middlewares.ForceHTML)
 }
