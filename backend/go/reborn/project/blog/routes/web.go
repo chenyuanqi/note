2bc2116ff72e536602119e89686ff20f37035bfd
chenyuanqi <chenyuanqi@outlook.com>
2022-05-12 18:21:47 +0800

    add: 添加会话控制

--- backend/go/reborn/project/blog/routes/web.go
+++ backend/go/reborn/project/blog/routes/web.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"blog/app/http/controllers"
+	"blog/app/http/middlewares"
+
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -34,6 +36,8 @@ func RegisterWebRoutes(r *mux.Router) {
 	auc := new(controllers.AuthController)
 	r.HandleFunc("/auth/register", auc.Register).Methods("GET").Name("auth.register")
 	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
+	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
+	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
 
 	// 静态资源
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
@@ -41,4 +45,9 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	// 中间件：强制内容类型为 HTML
 	// r.Use(middlewares.ForceHTML)
+
+	// --- 全局中间件 ---
+
+	// 开始会话
+	r.Use(middlewares.StartSession)
 }
