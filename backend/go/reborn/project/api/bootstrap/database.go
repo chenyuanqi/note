c80428ef650584cf4cfebd346420b12e15eab6c3
chenyuanqi <chenyuanqi90s@163.com>
2022-05-23 22:12:27 +0800

    add: 添加数据库日志

--- backend/go/reborn/project/api/bootstrap/database.go
+++ backend/go/reborn/project/api/bootstrap/database.go
@@ -4,6 +4,7 @@ import (
 	"api/app/models/user"
 	"api/pkg/config"
 	"api/pkg/database"
+	"api/pkg/logger"
 
 	"errors"
 	"fmt"
@@ -12,7 +13,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	// "gorm.io/gorm/logger"
 )
 
 // SetupDB 初始化数据库和 ORM
@@ -41,7 +42,8 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	// database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
