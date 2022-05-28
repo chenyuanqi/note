1368d2bce130161cd968d905792d28e1c80062d8
chenyuanqi <chenyuanqi90s@163.com>
2022-05-28 18:21:00 +0800

    发送短信验证码

--- backend/go/reborn/project/api/routes/api.go
+++ backend/go/reborn/project/api/routes/api.go
@@ -32,9 +32,10 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 
 			// 发送验证码
-            vcc := new(auth.VerifyCodeController)
-            // 图片验证码，需要加限流
-            authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
 		}
 	}
 }
