031105df3700e4a09cc1270b605889323f438bc6
chenyuanqi <chenyuanqi90s@163.com>
2022-05-30 11:22:33 +0800

    add: 发送邮件验证码

--- backend/go/reborn/project/api/pkg/verifycode/verifycode.go
+++ backend/go/reborn/project/api/pkg/verifycode/verifycode.go
@@ -6,8 +6,10 @@ import (
 	"api/pkg/config"
 	"api/pkg/helpers"
 	"api/pkg/logger"
+	"api/pkg/mail"
 	"api/pkg/redis"
 	"api/pkg/sms"
+	"fmt"
 
 	"strings"
 	"sync"
@@ -52,6 +54,32 @@ func (vc *VerifyCode) SendSMS(phone string) bool {
 		Template: config.GetString("sms.aliyun.template_code"),
 		Data:     map[string]string{"code": code},
 	})
+}
+
+// SendEmail 发送邮件验证码，调用示例：
+//         verifycode.NewVerifyCode().SendEmail(request.Email)
+func (vc *VerifyCode) SendEmail(email string) error {
+	// 生成验证码
+	code := vc.generateVerifyCode(email)
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && strings.HasSuffix(email, config.GetString("verifycode.debug_email_suffix")) {
+		return nil
+	}
+
+	content := fmt.Sprintf("<h1>您的 Email 验证码是 %v </h1>", code)
+	// 发送邮件
+	mail.NewMailer().Send(mail.Email{
+		From: mail.From{
+			Address: config.GetString("mail.from.address"),
+			Name:    config.GetString("mail.from.name"),
+		},
+		To:      []string{email},
+		Subject: "Email 验证码",
+		HTML:    []byte(content),
+	})
+
+	return nil
 }
 
 // CheckAnswer 检查用户提交的验证码是否正确，key 可以是手机号或者 Email
