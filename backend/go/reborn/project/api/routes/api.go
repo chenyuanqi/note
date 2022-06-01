d885246ee04d2844c1e03ebeb32a1ae140d9019e
chenyuanqi <chenyuanqi90s@163.com>
2022-06-01 11:00:05 +0800

    add: 添加限流中间件

--- backend/go/reborn/project/api/routes/api.go
+++ backend/go/reborn/project/api/routes/api.go
@@ -23,8 +23,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		c.String(http.StatusOK, "Hello guest")
 	})
 
-	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
 	{
 		// 注册一个路由
 		v1.GET("/", func(c *gin.Context) {
@@ -35,6 +38,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		})
 
 		authGroup := v1.Group("/auth")
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
+		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
