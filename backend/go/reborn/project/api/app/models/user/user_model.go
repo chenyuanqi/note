07d3da5d41e8fb002528553096499e581dcb55bc
chenyuanqi <chenyuanqi90s@163.com>
2022-06-01 10:28:40 +0800

    add: 通过手机号找回密码

--- backend/go/reborn/project/api/app/models/user/user_model.go
+++ backend/go/reborn/project/api/app/models/user/user_model.go
@@ -19,12 +19,18 @@ type User struct {
 	models.CommonTimestampsField
 }
 
+// ComparePassword 密码是否正确
+func (userModel *User) ComparePassword(_password string) bool {
+	return hash.BcryptCheck(_password, userModel.Password)
+}
+
 // Create 创建用户，通过 User.ID 来判断是否创建成功
 func (userModel *User) Create() {
 	database.DB.Create(&userModel)
 }
 
-// ComparePassword 密码是否正确
-func (userModel *User) ComparePassword(_password string) bool {
-	return hash.BcryptCheck(_password, userModel.Password)
+// Save 保存用户信息
+func (userModel *User) Save() (rowsAffected int64) {
+	result := database.DB.Save(&userModel)
+	return result.RowsAffected
 }
--- backend/go/reborn/project/api/routes/api.go
+++ backend/go/reborn/project/api/routes/api.go
@@ -57,6 +57,10 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/login/using-password", lgc.LoginByPassword)
 			// 刷新token
 			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+
+			// 重置密码
+			pwc := new(auth.PasswordController)
+			authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
 		}
 	}
 }
