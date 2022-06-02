8e47bb94ebaad856cdbeb5d85090fa226135652b
chenyuanqi <chenyuanqi90s@163.com>
2022-06-02 10:49:25 +0800

    add: 命令行模式

--- backend/go/reborn/project/api/go.mod
+++ backend/go/reborn/project/api/go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
@@ -46,6 +47,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
--- backend/go/reborn/project/api/main.go
+++ backend/go/reborn/project/api/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"api/app/cmd"
 	"api/bootstrap"
 	btsConfig "api/config"
 	"api/pkg/config"
+	"api/pkg/console"
 
-	"flag"
 	"fmt"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
 )
 
 func init() {
@@ -17,37 +19,43 @@ func init() {
 }
 
 func main() {
-	// TODO: error lens 配置
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   config.Get("app.name"),
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
 
-	// 配置初始化，依赖命令行 --env 参数
-	var env string
-	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
-	flag.Parse()
-	config.InitConfig(env)
+		// rootCmd 的所有子命令都会执行以下代码
+		PersistentPreRun: func(command *cobra.Command, args []string) {
+			// 配置初始化，依赖命令行 --env 参数
+			config.InitConfig(cmd.Env)
 
-	// 初始化 Logger
-	bootstrap.SetupLogger()
+			// 初始化 Logger
+			bootstrap.SetupLogger()
 
-	// 设置 gin 的运行模式，支持 debug, release, test
-	// release 会屏蔽调试信息，官方建议生产环境中使用
-	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
-	// 故此设置为 release，有特殊情况手动改为 debug 即可
-	gin.SetMode(gin.ReleaseMode)
+			// 初始化数据库
+			bootstrap.SetupDB()
 
-	// new 一个 Gin Engine 实例
-	router := gin.New()
+			// 初始化 Redis
+			bootstrap.SetupRedis()
 
-	// 初始化 DB
-	bootstrap.SetupDB()
-	// 初始化 Redis
-	bootstrap.SetupRedis()
-	// 初始化路由绑定
-	bootstrap.SetupRoute(router)
+			// 初始化缓存
+		},
+	}
 
-	// 运行服务
-	err := router.Run(":8000")
-	if err != nil {
-		// 错误处理，端口被占用了或者其他错误
-		fmt.Println(err.Error())
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+	)
+
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
+	// 执行主命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
 	}
 }
