13df795895517953f1618b2a59ffa54505163148
chenyuanqi <chenyuanqi90s@163.com>
2022-06-07 17:40:53 +0800

    update

--- backend/go/reborn/project/api/app/cmd/migrate.go
+++ backend/go/reborn/project/api/app/cmd/migrate.go
@@ -13,12 +13,6 @@ var CmdMigrate = &cobra.Command{
 	// 所有 migrate 下的子命令都会执行以下代码
 }
 
-var CmdMigrateUp = &cobra.Command{
-	Use:   "up",
-	Short: "Run unmigrated migrations",
-	Run:   runUp,
-}
-
 func init() {
 	CmdMigrate.AddCommand(
 		CmdMigrateUp,
@@ -34,6 +28,12 @@ func migrator() *migrate.Migrator {
 	migrations.Initialize()
 	// 初始化 migrator
 	return migrate.NewMigrator()
+}
+
+var CmdMigrateUp = &cobra.Command{
+	Use:   "up",
+	Short: "Run unmigrated migrations",
+	Run:   runUp,
 }
 
 func runUp(cmd *cobra.Command, args []string) {
