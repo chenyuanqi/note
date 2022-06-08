b22438be2552438e0c2f804f09cb52cce56c4041
chenyuanqi <chenyuanqi90s@163.com>
2022-06-08 18:31:48 +0800

    add: seed command

--- backend/go/reborn/project/api/pkg/seed/seeder.go
+++ backend/go/reborn/project/api/pkg/seed/seeder.go
@@ -4,20 +4,9 @@ package seed
 import (
 	"api/pkg/console"
 	"api/pkg/database"
-	"api/pkg/seed"
 
 	"gorm.io/gorm"
 )
-
-func Initialize() {
-
-	// 触发加载本目录下其他文件中的 init 方法
-
-	// 指定优先于同目录下的其他文件运行
-	seed.SetRunOrder([]string{
-		"SeedUsersTable",
-	})
-}
 
 // 存放所有 Seeder
 var seeders []Seeder
