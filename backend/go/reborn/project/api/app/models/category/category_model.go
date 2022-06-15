5891cf08e4f656da3ac1d34e3eacafd15e5c099e
chenyuanqi <chenyuanqi90s@163.com>
2022-06-15 18:31:49 +0800

    分类模型

---
+++ backend/go/reborn/project/api/app/models/category/category_model.go
@@ -0,0 +1,29 @@
+//Package category 模型
+package category
+
+import (
+	"api/pkg/database"
+)
+
+type Category struct {
+	models.BaseModel
+
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+
+	models.CommonTimestampsField
+}
+
+func (category *Category) Create() {
+	database.DB.Create(&category)
+}
+
+func (category *Category) Save() (rowsAffected int64) {
+	result := database.DB.Save(&category)
+	return result.RowsAffected
+}
+
+func (category *Category) Delete() (rowsAffected int64) {
+	result := database.DB.Delete(&category)
+	return result.RowsAffected
+}
---
+++ backend/go/reborn/project/api/database/migrations/2022_06_15_182150_add_categories_table.go
@@ -0,0 +1,32 @@
+package migrations
+
+import (
+	"api/app/models"
+	"api/pkg/migrate"
+
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2022_06_15_182150_add_categories_table", up, down)
+}
