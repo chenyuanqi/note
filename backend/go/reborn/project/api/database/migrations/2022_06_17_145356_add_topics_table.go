95afbeba15bb764f3798aaaeb52af69b2cd60453
chenyuanqi <chenyuanqi90s@163.com>
2022-06-17 14:55:29 +0800

    话题模型和迁移

---
+++ backend/go/reborn/project/api/database/migrations/2022_06_17_145356_add_topics_table.go
@@ -0,0 +1,45 @@
+package migrations
+
+import (
+	"api/app/models"
+	"api/pkg/migrate"
+
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+	}
+	type Category struct {
+		models.BaseModel
+	}
+
+	type Topic struct {
+		models.BaseModel
+
+		Title      string `gorm:"type:varchar(255);not null;index"`
+		Body       string `gorm:"type:longtext;not null"`
+		UserID     string `gorm:"type:bigint;not null;index"`
+		CategoryID string `gorm:"type:bigint;not null;index"`
+
+		// 会创建 user_id 和 category_id 外键的约束
+		User     User
+		Category Category
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Topic{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Topic{})
+	}
+
+	migrate.Add("2022_06_17_145356_add_topics_table", up, down)
+}
