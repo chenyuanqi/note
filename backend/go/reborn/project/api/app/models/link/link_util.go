e21657f9f435ca303cf60db16a8ae27991fb8f96
chenyuanqi <chenyuanqi90s@163.com>
2022-06-20 11:18:48 +0800

    缓存友情链接列表

--- backend/go/reborn/project/api/app/models/link/link_util.go
+++ backend/go/reborn/project/api/app/models/link/link_util.go
@@ -1,41 +1,66 @@
 package link
 
 import (
-    "api/pkg/app"
-    "api/pkg/database"
-    "api/pkg/paginator"
+	"api/pkg/app"
+	"api/pkg/cache"
+	"api/pkg/database"
+	"api/pkg/helpers"
+	"api/pkg/paginator"
 
-    "github.com/gin-gonic/gin"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Get(idstr string) (link Link) {
-    database.DB.Where("id", idstr).First(&link)
-    return
+	database.DB.Where("id", idstr).First(&link)
+	return
 }
 
 func GetBy(field, value string) (link Link) {
-    database.DB.Where("? = ?", field, value).First(&link)
-    return
+	database.DB.Where("? = ?", field, value).First(&link)
+	return
 }
 
 func All() (links []Link) {
-    database.DB.Find(&links)
-    return 
+	database.DB.Find(&links)
+	return
+}
+
+func AllCached() (links []Link) {
+	// 设置缓存 key
+	cacheKey := "links:all"
+	// 设置过期时间
+	expireTime := 120 * time.Minute
+	// 取数据
+	cache.GetObject(cacheKey, &links)
+
+	// 如果数据为空
+	if helpers.Empty(links) {
+		// 查询数据库
+		links = All()
+		if helpers.Empty(links) {
+			return links
+		}
+		// 设置缓存
+		cache.Set(cacheKey, links, expireTime)
+	}
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Link{}).Where(" = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Link{}).Where(" = ?", field, value).Count(&count)
+	return count > 0
 }
 
 func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
-    paging = paginator.Paginate(
-        c,
-        database.DB.Model(Link{}),
-        &links,
-        app.V1URL(database.TableName(&Link{})),
-        perPage,
-    )
-    return
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
 }
