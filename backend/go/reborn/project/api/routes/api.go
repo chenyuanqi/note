6ce33953dc30359d2e8df7ba00f00aa123efc63e
chenyuanqi <chenyuanqi90s@163.com>
2022-06-22 15:19:41 +0800

    API 域名和前缀

--- backend/go/reborn/project/api/routes/api.go
+++ backend/go/reborn/project/api/routes/api.go
@@ -6,6 +6,7 @@ import (
 	"api/app/http/controllers/api/v1/auth"
 	"api/app/http/middlewares"
 	auth_pkg "api/pkg/auth"
+	"api/pkg/config"
 	"api/pkg/response"
 
 	"net/http"
@@ -24,7 +25,15 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		c.String(http.StatusOK, "Hello guest")
 	})
 
-	v1 := r.Group("/v1")
+	// v1 := r.Group("/v1")
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
+
 	// 全局限流中间件：每小时限流。这里是所有 API （根据 IP）请求加起来。
 	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
 	// 测试时，可以调高一点。
