7c74222e029d20e955fe2622b435ba356b3a9358
chenyuanqi <chenyuanqi90s@163.com>
2022-07-09 11:53:23 +0800

    add

---
+++ backend/go/reborn/project/eddycjy_gin/cron/cron.go
@@ -0,0 +1,34 @@
+package cron
+
+import (
+	"eddycjy_gin/models"
+
+	"log"
+	"time"
+
+	"github.com/robfig/cron"
+)
+
+func main() {
+	log.Println("Starting...")
+
+	c := cron.New()
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllTag...")
+		models.CleanAllTag()
+	})
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllArticle...")
+		models.CleanAllArticle()
+	})
+
+	c.Start()
+
+	t1 := time.NewTimer(time.Second * 10)
+	for {
+		select {
+		case <-t1.C:
+			t1.Reset(time.Second * 10)
+		}
+	}
+}
