4ce948556208ff345fd68ba4d81bd4618f134fbf
chenyuanqi <chenyuanqi90s@163.com>
2022-11-26 16:54:14 +0800

    add: 初始化路由

--- backend/go/reborn/project/gohub_api/main.go
+++ backend/go/reborn/project/gohub_api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+    "fmt"
 
 	"github.com/gin-gonic/gin"
+
+	"gohub_api/bootstrap"
 )
 
 func main() {
@@ -12,27 +13,9 @@ func main() {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "vikey",
-		})
-	})
+	bootstrap.SetupRoute(r)
 
-	// 处理 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		// 获取标头信息的 Accept 信息
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
-
-	r.Run(":9999")
+	if err := r.Run(":9999"); err != nil {
+		fmt.Println(err.Error())
+	}
 }
