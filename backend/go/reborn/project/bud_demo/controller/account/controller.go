c07724e539ef39a3a75872f46978932b897411d7
chenyuanqi <chenyuanqi90s@163.com>
2023-01-03 15:42:05 +0800

    add: bud

---
+++ backend/go/reborn/project/bud_demo/controller/account/controller.go
@@ -0,0 +1,14 @@
+package account
+
+import (
+	context "context"
+)
+
+// Controller for accounts
+type Controller struct {
+}
+
+// Account struct
+type Account struct {
+	ID int `json:"id"`
+}
---
+++ backend/go/reborn/project/bud_demo/go.mod
@@ -0,0 +1,7 @@
+module change.me
+
+go 1.19
+
+require (
+	github.com/livebud/bud v0.2.5
+)
