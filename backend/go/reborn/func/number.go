3298613f3b74a25e1628bf6c4cbecf609907ead1
chenyuanqi <chenyuanqi90s@163.com>
2023-12-05 10:01:24 +0800

    update: 更新数字常用函数

--- backend/go/reborn/func/number.go
+++ backend/go/reborn/func/number.go
@@ -10,3 +10,15 @@ func RoundFloat(val float64, precision int) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
 }
+
+// 向上取整保留小数
+func CeilFloat(val float64, precision int) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Ceil(val*ratio) / ratio
+}
+
+// 向下取整保留小数
+func FloorFloat(val float64, precision int) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Floor(val*ratio) / ratio
+}
